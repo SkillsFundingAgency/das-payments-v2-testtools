0066ddee685c3bc2f582c3e62a9abd41395fcd7f
David-B-Read <david2.read@education.gov.uk>
2023-10-18 11:30:03 +0100

    PV2-3137 add page to send FCS handover complete message

--- src/FM36Tool/Spike.FM36Tool.Core/PeriodEndTask.cs
+++ src/FM36Tool/Spike.FM36Tool.Core/PeriodEndTask.cs
@@ -7,6 +7,7 @@
         PeriodEndRun,
         PeriodEndReports,
         PeriodEndStop,
-        PeriodEndIlrReprocessingJob
+        PeriodEndIlrReprocessingJob,
+        PeriodEndFcsHandOverCompleteJob
     }
 }
